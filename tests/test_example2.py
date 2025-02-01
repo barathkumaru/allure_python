ebe255ec70e878322313604c869aa430a7e79249
Barathkumar U <116795005+barathkumaru@users.noreply.github.com>
2025-02-01 22:17:09 +0530

    Create test_example2.py

---
+++ tests/test_example2.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_string():
+    """Check if the string matches"""
+    assert "hello" == "hello"
+
+def test_list():
+    """Check if list elements match"""
+    assert [1, 2, 3] == [1, 2, 3]
